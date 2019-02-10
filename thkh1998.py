110656c589440ff7a28e02142c89113a405361ca
tahakhany <39022302+tahakhany@users.noreply.github.com>
2019-02-10 15:46:22 +0330

    Update thkh1998.py

--- thkh1998.py
+++ thkh1998.py
@@ -44,21 +44,3 @@ def get_bot_user():
         username_id.append([answer['result'][0]['message']['from']['username'],answer['result'][0]['message']['from']['id']])
     return username_id
 
-#print(send_message('@thkh1998_ch','even the function is working',0))
-#print(set_chat_title('@thkh1998_ch','title has changed !'))
-#print(pin_chat_message('@thkh1998_ch',5))
-#print(get_chat('@thkh1998_ch'))
-#print(get_chat_member('@thkh1998_ch',?))
-#print (answer['result'][1])
-#print (answer['result'][1]['message']['from']['username'])
-#print (answer)
-#print (get_updates())
-#print (len(answer['result']))
-#print(get_bot_users())
-
-user_id=get_bot_user()
-print(user_id)
-bot_id='693605348:AAGNQ27QngVX8sVYPv-TfKY1tZI7nIQFnpg'
-url='https://api.telegram.org/bot%s/getUpdates'% bot_id
-answer=requests.post(url)
-print (answer)
